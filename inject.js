31a3c80a1351bf04d3378eeb6e5e2855b5cb574a
Kurt Lehnardt <gutbookvonnegut@gmail.com>
2020-12-16 21:34:13 -0700

    remove ads and annoying icons for imgur dot com

--- inject.js
+++ inject.js
@@ -16,12 +16,44 @@ function findHighestZIndex(){
 }
 
 function fixImgurCom(){
-  let ads= [...document.getElementsByClassName('Ad   up-show')]
-  ads.map(ad => ad.style.display = 'none')
-  let adsFooter = [...document.getElementsByClassName('Footer-wrapper')]
-  adsFooter.map(footer => footer.style.display = 'none')
+  removeImgurSelfAds()
+  removeImgurSponsoredAds()
+  removeImgurElementByClass('Accolade-background')
+  removeImgurElementByClass('NewPostsNotification')
 
 }
+
+function removeImgurSelfAds(){
+  let ads= [...document.getElementsByClassName('Ad   up-show')]
+  ads.map(ad => ad.style.display = 'none !important')
+  let adsFooter = [...document.getElementsByClassName('Footer-wrapper')]
+  adsFooter.map(footer => footer.style.display = 'none !important')
+}
+
+function removeImgurSponsoredAds(){
+  let galleryLinks = [...document.getElementsByTagName('a')]
+  galleryLinks.map(link => {
+    if (link.href.includes('ad')){
+      try { 
+        link.innerHTML = 'https://www.imgur.com/'
+        link.href = 'https://www.imgur.com/'
+        link.target = '#'
+      } catch {
+        console.log('ads already removed')
+      }
+    }
+  })
+}
+
+function removeImgurElementByClass(targetClass){
+  let galleryImages = [...document.getElementsByTagName('div')]
+  galleryImages.map(image => {
+    if (image.classList.contains(targetClass)){
+      image.remove()
+    }
+  })
+}
+
 
 function fixWeatherCom(){
   let elems = document.getElementsByTagName('div')
@@ -35,7 +67,7 @@ function fixWeatherCom(){
       elems[i].remove()
     }
   }
-  document.body.style.overflowY = 'scroll'
+  document.body.style.overflowY = 'scroll !important'
 }
 
 function fixNYTimesCom(){
@@ -48,13 +80,13 @@ function fixNYTimesCom(){
       elems[i].remove()
     }
     if (elems[i].style.background.includes('gradient')){
-      elems[i].style.background = 'none'
+      elems[i].style.background = 'none !important'
     }
     if (elems[i].style.overflow === 'hidden'){
-      elems[i].style.overflow = 'scroll'
+      elems[i].style.overflow = 'scroll !important'
     }
     if (elems[i].className === 'css-mcm29f'){
-      elems[i].style.overflow = 'scroll'
+      elems[i].style.overflow = 'scroll !important'
     }
   }
 }
@@ -62,17 +94,47 @@ function fixNYTimesCom(){
 function fixStandardNet(){
   console.log('fixing standard')
   let popup = document.getElementById('subscription-modal')
-  popup.style.display = 'none'
+  popup.style.display = 'none !important'
   let backdrop = document.getElementsByClassName('modal-backdrop')
-  backdrop[0].style.display = 'none'
+  backdrop[0].style.display = 'none !important'
   let overlay = document.getElementsByClassName('redacted-overlay')
-  overlay[0].style.display = 'none'
+  overlay[0].style.display = 'none !important'
   let subscription = document.getElementsByClassName('subscription-required')
-  subscription[0].style.display = 'none'
+  subscription[0].style.display = 'none !important'
   let hiddenParagraphs = [...document.getElementsByClassName('hide')]
   hiddenParagraphs.map(item => item.classList.remove('hide')) 
   let hiddenScroll = document.getElementsByClassName('modal-open')
-  hiddenScroll[0].style.overflow = 'auto'
+  hiddenScroll[0].style.overflow = 'auto !important'
+}
+
+function fixMediumCom(){
+  let currentPage = window.location.href
+  window.location.href = 'https://medium.com'
+  localStorage.clear()
+  sessionStorage.clear()
+  clearCookies()
+  console.log('cookies', document.cookie.split("; "))
+  localStorage.clear()
+  sessionStorage.clear()
+  window.location.reload(true)
+  window.location.href = currentPage
+}
+
+function clearCookies(){
+  var cookies = document.cookie.split("; ");
+  for (var c = 0; c < cookies.length; c++) {
+      var d = window.location.hostname.split(".");
+      while (d.length > 0) {
+          var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+          var p = location.pathname.split('/');
+          document.cookie = cookieBase + '/';
+          while (p.length > 0) {
+              document.cookie = cookieBase + p.join('/');
+              p.pop();
+          };
+          d.shift();
+      }
+  }
 }
 
 (()=>{
@@ -89,17 +151,36 @@ function fixStandardNet(){
     case 'https://www.standard.net':
       console.log('fixing standard') 
       fixStandardNet()
+    case 'https://medium.com':
+    case 'http://medium.com':
+    case 'medium.com':
+      fixMediumCom()
+      break
     default: 
       let bla = findHighestZIndex()
       bla.remove()
       console.log(bla)
   }
 
+  window.onscroll = function(e) {
+    let scrollUp = this.oldScroll > this.scrollY
+    if (scrollUp) fixImgurCom(scrollingUp)
+    this.oldScroll = this.scrollY;
+    if (!scrollUp) {
+      removeImgurElementByClass('Accolade-background')
+      removeImgurElementByClass('NewPostsNotification')
+    }
+  }
 
+  if (window.performance) {
+    if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+      fixImgurCom()
+    }
+  }
 
-  document.body.style.overflowY = 'scroll'
-  document.body.style.overflow = 'scroll'
+  document.body.style.overflowY = 'scroll !important'
+  document.body.style.overflow = 'scroll !important'
   const html = document.getElementsByTagName('html')[0]
-  html.style.overflow = 'auto'
+  html.style.overflow = 'auto !important'
   
 })()
